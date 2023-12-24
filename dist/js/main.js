9969fb15750fce52b48a1a26367026a955ede94e
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-12-24 20:56:43 +0200

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,7 @@
 const bars = document.getElementById("bars");
 const header = document.querySelector("header");
+let navLinks = document.querySelectorAll("nav ul li");
+let Sections = document.querySelectorAll("section");
 
 bars.addEventListener("click", () => {
   if (header.classList.contains("hidden")) {
@@ -15,4 +17,37 @@ bars.addEventListener("click", () => {
     });
     header.classList.add("hidden");
   }
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target.tagName != "UL" && e.target.tagName != "I") {
+    if (!header.classList.contains("hidden")) {
+      header.animate([{ width: "33.3333%" }, { width: "0" }], {
+        duration: 300,
+        fill: "backwards",
+      });
+      header.classList.add("hidden");
+    }
+  }
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+    });
+    link.classList.add("active");
+  });
+});
+
+Sections.forEach((section) => {
+  section.addEventListener("mouseover", () => {
+    let sectionId = section.getAttribute("id");
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("data-link") == sectionId) {
+        link.classList.add("active");
+      }
+    });
+  });
 });
